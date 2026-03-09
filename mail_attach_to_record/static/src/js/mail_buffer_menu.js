3adc26238b6dda5c1d40200054dc8626a1f6e6ba
Liam Noonan <ljmnoonan@gmail.com>
2026-03-09 07:56:28 +0000

    Now it updates auto

--- mail_attach_to_record/static/src/js/mail_buffer_menu.js
+++ mail_attach_to_record/static/src/js/mail_buffer_menu.js
@@ -1,46 +1,74 @@
-import {Component, useState} from "@odoo/owl";
+import {Component, useState, onWillStart, useEffect} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
 import {useDiscussSystray} from "@mail/utils/common/hooks";
+import {useDropdownState} from "@web/core/dropdown/dropdown_hooks";
 import {Dropdown} from "@web/core/dropdown/dropdown";
 import {DropdownItem} from "@web/core/dropdown/dropdown_item";
 import {NotificationItem} from "@mail/core/public_web/notification_item";
+import {user} from "@web/core/user";
 
 export class MailBufferMenu extends Component {
     static template = "mail_attach_to_record.MailBufferMenu";
     static components = {Dropdown, DropdownItem, NotificationItem};
 
     setup() {
+        super.setup();
         this.discussSystray = useDiscussSystray();
+        this.dropdown = useDropdownState();
         this.orm = useService("orm");
         this.action = useService("action");
         this.notification = useService("notification");
+        this.busService = useService("bus_service");
+        this.store = useService("mail.store");
         this.state = useState({
             messages: [],
             isLoading: false,
         });
+
+        onWillStart(() => this.fetchMessages());
+
+        useEffect(
+            () => {
+                const unsubscribe = this.busService.subscribe(
+                    "mail.plugin.log_mail_raw",
+                    () => this.fetchMessages()
+                );
+                return unsubscribe;
+            },
+            () => []
+        );
     }
 
-    async beforeOpen() {
+    async fetchMessages() {
         this.state.isLoading = true;
         try {
             this.state.messages = await this.orm.searchRead(
                 "mail.message",
-                [["model", "=", "mail.buffer"]], // Domain
+                [
+                    ["model", "=", "mail.buffer"],
+                    ["create_uid", "=", user.userId],
+                ],
                 ["id", "subject", "email_from", "date"], // Fields
-                {limit: 20, order: "date desc"}
+                {limit: 100, order: "date desc"}
             );
         } finally {
             this.state.isLoading = false;
         }
     }
 
+    async beforeOpen() {
+        await this.fetchMessages();
+    }
+
     async attachMessage(message) {
-        const currentController = this.action.currentController;
-        const props = currentController ? currentController.props : {};
-        // Attempt to find the active record in the current view
-        const resModel = props.resModel || props.res_model;
-        const resId = props.resId || props.res_id;
+        const controller = this.action.currentController;
+        const resModel = controller?.action.res_model;
+        const resIdRaw = controller?.currentState?.resId;
+        const resId = resIdRaw && typeof resIdRaw === "number" ? resIdRaw : undefined;
+
+        this.dropdown.close();
+
         if (resModel && resId) {
             try {
                 await this.orm.call("mail.buffer", "assign_message", [
@@ -56,10 +84,10 @@ export class MailBufferMenu extends Component {
                     (m) => m.id !== message.id
                 );
                 // Refresh the view to show the new message in the chatter
-                await this.action.doAction({
-                    type: "ir.actions.client",
-                    tag: "reload",
-                });
+                const thread = this.store.Thread.get({model: resModel, id: resId});
+                if (thread) {
+                    await thread.loadAround(message.id);
+                }
             } catch (e) {
                 this.notification.add("Failed to attach email.", {
                     type: "danger",
@@ -67,7 +95,7 @@ export class MailBufferMenu extends Component {
             }
         } else {
             this.notification.add(
-                "Please open a record form view to attach the email.",
+                "Please open a record form view to attach the email.\nIf open already, try reloading the page.",
                 {type: "warning"}
             );
         }
